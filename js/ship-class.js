47c2bbc2ab8dd3521a4e9caf73239abaac9cb227
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-07-05 21:50:27 +0200

    move methods 'inBoard,_index,isOverlaped,drawShip' to Board Class. This is much better place for this method :)

--- js/ship-class.js
+++ js/ship-class.js
@@ -16,14 +16,7 @@ const directions = [
 
 class Ship {
 
-    constructor(_board, shipType, dir) {
-        /*
-            To jest dość mocno uogólniona metoda
-            Pozwala stworzyć statek w dowolnej pozycji (x,y) na mapie (board),
-            o dowolnej ilości masztów (mast)
-            oraz jego ułożeniu (set)
-        */
-        this.board = _board;    // referencja do mapy
+    constructor(shipType, dir) {
         this.x = 0;            // położenie x,y
         this.y = 0;            //
         this.shipType = shipType;    // ilość masztów
@@ -31,91 +24,22 @@ class Ship {
 
         // ...inicjujemny maszty statku :)
         this.masts = [];           // tablica istnienia masztów statku
-
         for (let i = 0; i < this.shipType; i++) {
             this.masts.push(true); // maszt istnieje :D
         }
     }
 
     clone() {
-        let newship = new Ship(this.board, this.shipType, this.dir);
+        let newship = new Ship(this.shipType, this.dir);
         newship.x = this.x;
         newship.y = this.y;
         return newship;
-    }
-
-    inBoard() {
-        let mastsCount = this.masts.length;
-
-        const _inPool = function (x, y, board) {
-            return ((x >= 0 && x < board.width &&
-                y >= 0 && y < board.height));
-        }
-
-        let x1 = this.x,
-            y1 = this.y,
-            x2 = this._pos(mastsCount - 1).x,
-            y2 = this._pos(mastsCount - 1).y;
-
-        return (_inPool(x1, y1, this.board) && _inPool(x2, y2, this.board));
     }
 
     _pos(i) {
         let nx = this.x + directions[this.dir].x * i,
             ny = this.y + directions[this.dir].y * i
         return { x: nx, y: ny };
-    }
-
-    _index(x, y) {
-        return x + y * this.board.width;
-    }
-    isOverlaped() {
-        let nx, ny, id, isOverlap = false;
-
-        for (let i = 0; i < this.masts.length; i++) {
-            ({ x: nx, y: ny } = this._pos(i));
-
-            for (let shipID in this.board.ships) {
-                if (this.board.ships[shipID].isHit(nx, ny) !== false) {
-                    id = this._index(nx, ny);
-                    this.board.boardCells[id].addClass('error');
-                    isOverlap = true;
-                }
-            }
-        }
-        return isOverlap;
-    }
-
-    draw(mode = 0) {
-        let nx, ny, id, cell;
-
-        for (let i = 0; i < this.masts.length; i++) {
-            ({ x: nx, y: ny } = this._pos(i));
-
-            id = this._index(nx, ny);
-            cell = this.board.boardCells[id];
-
-            if (mode)
-                cell.addClass('mast')
-            else
-                cell.addClass('edit');
-
-            if (!this.masts[i]) {
-                cell.addClass('mast hit');
-            }
-        }
-    }
-
-    clear() {
-        let nx, ny, id;
-        for (let i = 0; i < this.masts.length; i++) {
-            ({ x: nx, y: ny } = this._pos(i));
-
-            id = this._index(nx, ny);
-            let cell = this.board.boardCells[id];
-
-            cell.removeClass('mast hit');
-        }
     }
 
     isHit(hitx, hity) {

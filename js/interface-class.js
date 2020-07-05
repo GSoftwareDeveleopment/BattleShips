b67f2d8b5afaa21bd1264329246c2f9045fef397
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-07-05 23:19:15 +0200

    Enabling interface connection to any DOM element (not only to the Screen class)

--- js/interface-class.js
+++ js/interface-class.js
@@ -1,7 +1,11 @@
 class Interface {
-    constructor(screen) {
-        this._containerID = screen.containerID;
-        this._screen = screen.screen;
+    constructor(container) {
+        if (container instanceof Screen) {
+            this._containerID = container.containerID;
+            this._container = container.container;
+        } else {
+            this._container = container;
+        }
         this._elements = new Array();
     }
 
@@ -9,7 +13,7 @@ class Interface {
         console.groupCollapsed(`Initialize interface '${this._containerID}'.'${group}'...`)
 
         this[group] = new Array();
-        this._elements[group] = this._screen.find(selector);
+        this._elements[group] = this._container.find(selector);
         console.log(`${this._elements.length} element(s) in interface found`);
 
         this._elements[group].each((index, el) => {

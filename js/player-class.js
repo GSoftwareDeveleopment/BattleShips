5039184fcbc126ed29695767da45fcc345b7632d
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-07-06 00:41:52 +0200

    Remove method from class

--- js/player-class.js
+++ js/player-class.js
@@ -29,26 +29,4 @@ class Player {
         this.name = null;
         this.mode = null;
     }
-
-    createShips() {
-        let newship;
-        switch (this.mode) {
-            case 0: // ustawianie statków "przez komputer"
-                for (let j = 0; j < 4; j++) { // rodzaj statku (0- jednomasztowiec; 1- dwumasztowiec; ...)
-                    for (let i = 0; i < (4 - j); i++) { // ilość statków (im większy statek, tym mniejsza ich ilość)
-                        do {
-                            let dir = rand(0, 1);  // losowanie orientacji statku
-                            let x = rand(0, 9),    // losowanie pozycji statku
-                                y = rand(0, 9);
-                            newship = new Ship(this.board, x, y, j, dir)
-                        } while (newship); // jeżeli statek został bez przeszkód ustawiony, wyjdź z pętli
-                        this.board.ships.push(newship); // dodaj nowy statek do listy
-                    }
-                }
-                break;
-            case 1: // ustawienie statków przez człowieka
-                // tu logika dla ustawiania statków przez gracza
-                break;
-        }
-    }
 }

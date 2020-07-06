600adb48afd5f3e39dc220b8fc5a775314a21be6
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-07-06 11:00:22 +0200

    Adaptation of the interface.

--- js/dockyard-class.js
+++ js/dockyard-class.js
@@ -1,14 +1,15 @@
 class Dockyard extends Interface {
 
     constructor(dockyardData, container) {
-        super(container);
+        let c = $('<ul id="dockyard-list" />'); // utworzenie nowego elementu DOM
+        super(c);
 
         this.dockyard = dockyardData;
         for (let i = 0; i < this.dockyard.length; i++) {
             let shipdata = this.dockyard[i];
 
             if (!shipdata.mainField) { // main list item element
-                shipdata.mainField = $(`<li data-shiptype="${shipdata.shipType}"/>`);
+                shipdata.mainField = $(`<li id="ship-${i}" data-shiptype="${shipdata.shipType}"/>`);
                 this._container.append(shipdata.mainField);
             }
 
@@ -40,8 +41,11 @@ class Dockyard extends Interface {
             }
         }
 
+        // dodanie elementu DOM do wyznaczonego kontenera 'container'
+        container.append(this._container);
+
         // obsługa zdarzeń list statków (dockyard-list) (lewy przycisk myszy)
-        this.interface.build('fieldset ul li', 'dockyard-items')
+        this.build('li', 'dockyard-items')
             .on('click', (e) => { this.selectShip(e); });
 
         this.selectedShip = null;
@@ -51,7 +55,11 @@ class Dockyard extends Interface {
         this.update();
     }
 
-    /* przygotowanie listy statków (dockyard-list) */
+    remove() {
+        this._container.detach();
+    }
+
+    /* aktualizacja listy statków (dockyard-list) */
     update() {
         for (let i = 0; i < this.dockyard.length; i++) {
             let shipdata = this.dockyard[i];
@@ -66,7 +74,7 @@ class Dockyard extends Interface {
         }
     }
 
-    //
+    // metoda pobiera dane dotyczące statku (z this.dockyard), na podstawie jego typu
     getShipData(shipType) {
         let shipdata;
         for (let i = 0; i < this.dockyard.length; i++) {
@@ -80,26 +88,29 @@ class Dockyard extends Interface {
 
     // obsługa listy statków gracza (dockyard-lisy)
     selectShip(e) {
-        this._lastSelectedShipType = this.selectedShipType;
 
-        this.unselectShip();
+        if (this.selectedShip) {
+            this.selectedShip.removeClass('selected');
+        }
 
         this.selectedShip = $(e.currentTarget);
         this.selectedShipType = this.selectedShip.data('shiptype');
-        const shipData = this.findShipList(this.selectedShipType);
+        const shipData = this.getShipData(this.selectedShipType);
 
         if (this.selectedShipType !== this._lastSelectedShipType) {
             if (shipData.Q > 0) {
-                if (this.onSelect) this.onSelect(shipType);
+                if (this.onSelect) this.onSelect(this.selectedShipType);
                 this.selectedShip.addClass('selected');
             } else {
-                if (this.onNotAvailable) this.onNotAvailable(shipType);
+                if (this.onNotAvailable) this.onNotAvailable(this.selectedShipType);
                 this.selectedShip = null;
             }
         } else {
-            if (this.onUnselect) this.onUnselect(shipType)
+            if (this.onUnselect) this.onUnselect(this.selectedShipType);
             this.selectedShip = null;
+            this.selectedShipType = null;
         }
+        this._lastSelectedShipType = this.selectedShipType;
     }
 
     unselectShip() {
@@ -107,7 +118,7 @@ class Dockyard extends Interface {
             this.selectedShip.removeClass('selected');
         }
         this.selectedShip = null;
-        if (this.onUnselect) this.onUnselect(shipType)
+        if (this.onUnselect) this.onUnselect(this.selectedShipType)
     }
 
 }

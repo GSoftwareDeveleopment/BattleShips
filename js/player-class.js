9797afbebd80d674a739ef370c717519d777d986
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-07-08 11:39:32 +0200

    Removing the 'isPlayerDone' property and creating boards: setting up ships and battles.

--- js/player-class.js
+++ js/player-class.js
@@ -5,7 +5,8 @@ class Player {
         this.id = id;
         this.name = id;
 
-        this.isPlayerDone = false; // znacznik oznajmiający koniec czynności gracza
+        this.score = 0;
+        this.moves = 0;
 
         this.reset();
     }
@@ -13,15 +14,19 @@ class Player {
     reset() {
         this.score = 0;
         this.moves = 0;
-        this.isPlayerDone = false;
 
+        console.log('- setting player dockyard');
         this.dockyard = JSON.parse(JSON.stringify(default_dockyard));
         // rozszerzenie listy o listę referencji statków znajdujących się już na mapie
         for (let id = 0; id < this.dockyard.length; id++) {
             this.dockyard[id].list = [];
         }
+
         let boardSize = this.game.settings.boardSize;
-        this.board = new Board(this, boardSize, boardSize); // inicjowanie planszy gracza
+        this.board = new Board(this, boardSize, boardSize); // plansza ze statkami gracza
+        this.board.setEditBoard();
+        this.battleBoard = new Board(this, boardSize, boardSize); // plansza bitwy gracza
+        this.battleBoard.setGameBoard();
     }
 
     removePlayer() {

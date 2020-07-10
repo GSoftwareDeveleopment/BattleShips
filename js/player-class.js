598c633672f4fb3863031d17ab182c4a8737b5d2
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-07-10 14:58:57 +0200

    add some console messages; add shared methods 'prepare2SetupShips', 'setupShipsDone', 'prepare2Battle', 'beginTurn', 'fire', 'endTurn'

--- js/player-class.js
+++ js/player-class.js
@@ -8,14 +8,16 @@ class Player {
         this.score = 0;
         this.moves = 0;
 
+        console.groupCollapsed(`Player class: Initialize player '${this.id}'...`);
         this.reset();
+        console.groupEnd();
     }
 
     reset() {
         this.score = 0;
         this.moves = 0;
 
-        console.log('- setting player dockyard');
+        console.log('Player class: - setting player dockyard');
         this.dockyard = JSON.parse(JSON.stringify(default_dockyard));
         // rozszerzenie listy o listę referencji statków znajdujących się już na mapie
         for (let id = 0; id < this.dockyard.length; id++) {
@@ -37,4 +39,44 @@ class Player {
         this.name = null;
         this.mode = null;
     }
+
+    //
+    //
+    //
+
+    prepare2SetupShips(_setupShips) {
+        console.log(`Player class: Preparing player '${this.id} to setups shpis...`)
+        this.setupShips = _setupShips;
+    }
+
+    setupShipsDone() {
+        console.log(`Player class: Player '${this.id} are done setup shpis...`)
+        this.board.clear();
+        this.board.hideBoard();
+    }
+
+    //
+    //
+    //
+
+    prepare2Battle(_battle) {
+        console.log(`Player class: Preparing player '${this.id} to battle...`)
+        this.battle = _battle;
+    }
+
+    beginTurn() {
+        console.log(`Player class: Player '${this.id} begin turn...`)
+        this.battleBoard.showBoard();
+    }
+
+    fire(x, y) {
+        const letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        console.log(`Player class: Player '${this.id} give a shot on point ${letter.charAt(x)}${y}`)
+    }
+
+    endTurn() {
+        console.log(`Player class: Player '${this.id} end turn...`)
+        this.battleBoard.hideBoard();
+    }
+
 }

c1c86ac46f298d13c2f44cf91d03e2e074a9ef66
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-07-10 19:43:56 +0200

    Moving files to the Interface directory

--- js/progressbar-class.js
+++
@@ -1,8 +0,0 @@
-class ProgressBar extends Interface {
-    constructor(contaienr) {
-        let c = $('<div id="progressbar" />'); // utworzenie nowego elementu DOM
-        super(c);
-
-        container.append(this._container);
-    }
-}

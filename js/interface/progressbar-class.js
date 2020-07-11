7dbd069115c1b52eb446a700abee9d580a837e68
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-07-11 11:36:23 +0200

    implementation of interface support

--- js/interface/progressbar-class.js
+++ js/interface/progressbar-class.js
@@ -3,6 +3,16 @@ class ProgressBar extends Interface {
         let c = $('<div id="progressbar" />'); // utworzenie nowego elementu DOM
         super(c);
 
-        container.append(this._container);
+        this._progress = $('<div class="progress"/>)')
+            .appendTo(this._container);
+        contaienr.append(this._container);
+    }
+
+    set(progressValue) {
+        this._progress.css('width', parseInt(progressValue * 100) + '%');
+    }
+
+    remove() {
+        this._container.detach();
     }
 }
